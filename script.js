14de1175957dddad4f7f67355770aec5a8299a2a
Versantos <veronicasilvamagalhaes@gmail.com>
2022-01-27 00:38:17 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+let pessoal=[
+    { 
+        signo: "gemeos",
+        idade: 30,
+        time:"Vasco",
+
+    },
+]
+pessoal.defeito ="teimosia",
+console.log(pessoal)
+
+delete pessoal.defeito
+console.log(pessoal)
+
+
+let objt=[
+    { 
+        nome: "Maria",
+        idade: 30,
+        telefone: 210000,
+        amigos: ["Aparecida","José","João","Tawo"]
+
+    },
+    {
+        nome: "Neuza",
+        idade: 30,
+        telefone: 214789,
+        amigos: ["Eduarda","Peri","Dawo","Luiz"]
+        
+    },
+    {
+        nome: "Isabela",
+        idade: 25,
+        telefone: 256879,
+        amigos: ["Priscila","Beatriz","Claudia","Carolina"]
+
+    },
+    {
+        nome:"Vany",
+        idade: 24,
+        telefone: 256987,
+        amigos: ["Igor","Dudu","Mari","Paola"]
+
+    },
+    {
+        nome: "Igor",
+        idade: 20,
+        telefone: 569874,
+        amigos: ["Victor","Adriana","Ana","Bia"]
+
+    }
+]
+ console.log(objt [0].amigos[0])
+ console.log(objt [1].amigos[0])
+ console.log(objt [2].amigos[0])
+ console.log(objt [3].amigos[0])
+ console.log(objt [4].amigos[0])
